d78171dfa7e418653037097f37eb04605fcd7cd4
egorycheva28 <egor.masha28@list.ru>
2026-04-30 19:06:31 +0700

    fix 6 bug

--- src/pages/Operators/components/NewOperatorDialog/constants/NewOperatorShema.ts
+++ src/pages/Operators/components/NewOperatorDialog/constants/NewOperatorShema.ts
@@ -2,7 +2,7 @@ import * as z from 'zod';
 
 export const newOperatorSchema = z.object({
     fullName: z.string().min(3, "Минимум 3 символа").max(255, "Максимум 255 символов"),
-    password: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/, "Введите корректный пароль"),
+    password: z.string().regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$/, "Пароль должен быть от 8 до 20 символов, содержа минимум 1 английскую букву и 1 цифру"),
     phone: z.string().regex(/^(?:\+?7|8)\d{10}$/, "Введите корректный номер телефона"),
     username: z.string().min(3, "Минимум 3 символа").max(100, "Максимум 100 символов")
 })
